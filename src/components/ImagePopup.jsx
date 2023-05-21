8ef3293a044b7cc2662839fdcc5d76a4bc1036a6
Elena Yadryshnikova <elenaya014@gmail.com>
2023-05-22 01:03:20 +0700

    feat: image popup

--- src/components/ImagePopup.jsx
+++ src/components/ImagePopup.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 
-export const ImagePopup = () => {
+export const ImagePopup = ({ card, onClose }) => {
+    let className = 'popup popup_type_image';
+    if (card) {
+        className += ' popup_active';
+    }
+
     return (
-        <div className="popup popup_type_image">
+        <div className={className}>
             <div className="popup__container">
-                <button className="popup__close-cross" type="button" aria-label="Закрыть"></button>
+                <button className="popup__close-cross" type="button" aria-label="Закрыть" onClick={onClose}></button>
                 <figure className="img-figure">
-                    <img className="img-figure__image" />
+                    <img className="img-figure__image" src={card?.link} alt={`Фотография: ${card?.name}.`} />
                     <figcaption className="img-figure__caption">
-                        <h2 className="img-figure__description"></h2>
+                        <h2 className="img-figure__description">{card?.name}</h2>
                     </figcaption>
                 </figure>
             </div>
