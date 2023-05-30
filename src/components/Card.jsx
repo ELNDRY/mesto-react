2b6b03f70070d32c22ba2ca0ee4b9944c3d830f9
Elena Yadryshnikova <elenaya014@gmail.com>
2023-05-30 23:07:04 +0700

    feat: use context in Main

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
 export const Card = ({ card, onCardClick }) => {
 
+    const currentUser = useContext(CurrentUserContext);
+
     const handleCardClick = () => onCardClick(card);
+
+    const isOwn = card.owner._id === currentUser._id;
+
+    const isLiked = card.likes.some(i => i._id === currentUser._id);
+
+    const cardLikeButtonClassName = (
+        `element__like ${isLiked && 'element__like_active'}`
+    )
 
     return (
         <li className="element">
-            <button className="element__delete" type="button" aria-label="Удалить" />
-            <img className="element__image" src={card?.link} alt={`Фотография: ${card?.name}.`} onClick={handleCardClick}/>
+            {isOwn && <button className="element__delete" type="button" aria-label="Удалить" />}
+            <img className="element__image" src={card?.link} alt={`Фотография: ${card?.name}.`} onClick={handleCardClick} />
             <div className="element__container">
                 <h2 className="element__text">{card?.name}</h2>
                 <div className="element__wrapper">
-                    <button className="element__like" type="button" aria-label="Нравится" />
+                    <button className={cardLikeButtonClassName} type="button" aria-label="Нравится" />
                     <span className="element__counter">{card?.likes.length}</span>
                 </div>
             </div>
